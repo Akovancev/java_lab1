39892fa13bde7339d37931c98cdb17eab68759d9
Akovancev <apm120700@gmail.com>
2021-02-23 22:48:32 +0300

    Delete elements

--- src/com/company/List.java
+++ src/com/company/List.java
@@ -3,9 +3,14 @@ package com.company;
 public class List {
     private int data;
     private List next = null;
+    private int count;
 
     public int getData() {
         return this.data;
+    }
+
+    public int getCount() {
+        return count;
     }
 
     public List getNext() {
@@ -16,23 +21,48 @@ public class List {
         this.data = data;
     }
 
-    public List() {
-        data = 0;
-        next = null;
-    }
-
-    public List(int data) {
-        this.data = data;
-    }
-
-    public List(int data, List next) {
-        this.data = data;
+    public void setNext(List next) {
         this.next = next;
     }
 
-    public void AddToBegin(int data) {
-        List l = new List(this.data, this.next);
-        this.data = data;
-        this.next = l;
+    public List() {
+        setData(0);
+        next = null;
+        count = 1;
     }
+
+    public List(int data) {
+        setData(data);
+        count = 1;
+    }
+
+    public List(int data, List next) {
+        setData(data);
+        setNext(next);
+        count = 1;
+    }
+
+    public void Add(int data) {
+        List l = new List(this.getData(), this.getNext());
+        setData(data);
+        setNext(l);
+        count++;
+    }
+
+    public void Delete() {
+        if (this.getCount() == 1) {
+            setData(0);
+            setNext(null);
+            count = 0;
+            return;
+        }
+        if (this.getCount() == 0) {
+            return;
+        }
+        List l = this.getNext();
+        setData(l.getData());
+        setNext(l.getNext());
+        count--;
+    }
+
 }
